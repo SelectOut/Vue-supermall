58526a5b596de426be9f286e08ba33a32dd6f24a
SelectOut <2449193462@qq.com>
2021-08-14 22:13:09 +0800

    '最后的版本'

---
+++ src/network/request.js
@@ -0,0 +1,45 @@
+import axios from "axios";
+import { Toast } from "vant";
+import Loading from "../store/index";
+
+// 本接口为测试接口,真实接口请联系coderwhy001
+const url = "xxx";
+
+let config = {
+    baseURL:'http://152.136.185.210:7878/api/m5',
+  timeout: 10000
+};
+
+const _axios = axios.create(config);
+
+// 请求拦截
+_axios.interceptors.request.use(
+  req => {
+    // 当getters里面的isLoading为true再显示请求加载
+    if (Loading.getters.isLoading) {
+      Toast.loading({
+        forbidClick: true,
+        message: "加载中..."
+      });
+    }
+    return req;
+  },
+  err => {
+    return Promise.reject(err);
+  }
+);
+
+// 响应拦截
+_axios.interceptors.response.use(
+  res => {
+    Toast.clear();
+    return res.data;
+  },
+  err => {
+    Toast.clear();
+    return Promise.reject(err);
+  }
+);
+
+// 全局注册axios
+window.axios = _axios;
